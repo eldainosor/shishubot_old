b3e9c5eee7fcc23099b333ebce8b8d169771d77c
merothh <merothh@gmail.com>
2019-05-29 21:14:08 +0530

    shishu_bot: reply to /start in PM

--- shishu_bot/__main__.py
+++ shishu_bot/__main__.py
@@ -1,3 +1,18 @@
 from shishu_bot.config import Config
+from telegram.ext import Updater, CommandHandler
 
 TOKEN = Config.API_KEY
+
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
+
+# callback function for start handler
+def start_callback(bot, update):
+    if update.message.chat.type=="private":
+        bot.send_message(chat_id=update.message.chat_id, text="Hi there")
+
+start_handler = CommandHandler('start', start_callback)
+
+dispatcher.add_handler(start_handler)
+
+updater.start_polling()
