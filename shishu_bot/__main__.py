0efec0f3bf1d16ad2ae687b82c1360e7cee88fd9
merothh <merothh@gmail.com>
2019-07-01 23:51:43 +0530

    shishu_bot: process data from raw file

--- shishu_bot/__main__.py
+++ shishu_bot/__main__.py
@@ -1,6 +1,6 @@
 from shishu_bot.config import Config
 from telegram.ext import Updater, CommandHandler
-import logging, urllib.request
+import logging, re, urllib.request
 
 TOKEN = Config.API_KEY
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -24,6 +24,13 @@ def start_callback(bot, update):
 def device_callback(bot, update, args):
     codename = args[0]
     ota_raw = get_ota_raw(codename)
+
+    maintainer = re.findall(r"\\nmaintainer: (.*?)\\n", ota_raw)[0]
+    filename = re.findall(r"\\nfilename: (.*?)\\n", ota_raw)[0]
+    fullname = re.findall(r"\\nfullname: (.*?)\\n", ota_raw)[0]
+    xdathread = re.findall(r"\\nxdathread: (.*?)\\n", ota_raw)[0]
+    latest = "http://downloads.sourceforge.net/project/bootleggersrom/builds/"+codename+"/"+filename
+    builds = "http://downloads.sourceforge.net/project/bootleggersrom/builds/"+codename
 
 start_handler = CommandHandler('start', start_callback)
 device_handler = CommandHandler('device', device_callback, pass_args=True)
