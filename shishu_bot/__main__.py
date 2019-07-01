24d9ca70dabaffdf410568e670e6df4e6f084dd8
merothh <merothh@gmail.com>
2019-07-01 16:31:15 +0530

    shishu_bot: add a basic device handler
    * just reply the first word passed for now

--- shishu_bot/__main__.py
+++ shishu_bot/__main__.py
@@ -13,8 +13,14 @@ def start_callback(bot, update):
     if update.message.chat.type=="private":
         bot.send_message(chat_id=update.message.chat_id, text="Hi there")
 
+# callback function for device handler
+def device_callback(bot, update, args):
+    bot.send_message(chat_id=update.message.chat_id, text=args[0])
+
 start_handler = CommandHandler('start', start_callback)
+device_handler = CommandHandler('device', device_callback, pass_args=True)
 
 dispatcher.add_handler(start_handler)
+dispatcher.add_handler(device_handler)
 
 updater.start_polling()
