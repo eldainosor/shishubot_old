c6a8831daa37974f4bae7352a2fce1ba42713939
merothh <merothh@gmail.com>
2019-07-01 23:50:11 +0530

    shishu_bot: get raw file for device passed

--- shishu_bot/__main__.py
+++ shishu_bot/__main__.py
@@ -1,12 +1,19 @@
 from shishu_bot.config import Config
 from telegram.ext import Updater, CommandHandler
-import logging
+import logging, urllib.request
 
 TOKEN = Config.API_KEY
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
+
+def get_ota_raw(codename):
+    ota_url = "https://raw.githubusercontent.com/BootleggersROM-Devices/BootleggersROM-Devices.github.io/master/_devices/"+codename+".md"
+    ota_req = urllib.request.Request(ota_url)
+    ota_resp = urllib.request.urlopen(ota_req)
+    ota_raw = ota_resp.read()
+    return str(ota_raw)
 
 # callback function for start handler
 def start_callback(bot, update):
@@ -15,7 +22,8 @@ def start_callback(bot, update):
 
 # callback function for device handler
 def device_callback(bot, update, args):
-    bot.send_message(chat_id=update.message.chat_id, text=args[0])
+    codename = args[0]
+    ota_raw = get_ota_raw(codename)
 
 start_handler = CommandHandler('start', start_callback)
 device_handler = CommandHandler('device', device_callback, pass_args=True)
